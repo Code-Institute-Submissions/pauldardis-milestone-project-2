424ecb357fc31ba33bf2891fd5fd924f5a0d0008
Ubuntu <ubuntu@ip-172-31-90-189.ec2.internal>
2019-10-15 11:33:58 +0000

    adding Candidate Data Table

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -10,8 +10,8 @@ function makeGraphs(error, electionData) {
     region_selector(ndx);
     constituency_selector(ndx);
     party_first_preference_graphs(ndx);
-    // candidate_graphs(ndx);
-    // show_data_table(ndx);
+    candidate_graphs(ndx);
+    show_data_table(ndx);
     show_count_candidate_fg(ndx, "F", "#count-of-women-candidate-fg");
     show_count_candidate_fg(ndx, "M", "#count-of-men-candidate-fg");
     show_count_elected_fg(ndx, "F", "#count-of-women-elected-fg");
@@ -596,4 +596,101 @@ function party_first_preference_graphs(ndx) {
             })
         })
         .group(group);
+}
+
+function candidate_graphs(ndx) {
+    var dim = ndx.dimension(dc.pluck('Party_Abbreviation'));
+    var group = dim.group().reduceSum(dc.pluck('Seat'));
+
+    dc.rowChart("#candidate-graph")
+        .width(500)
+        .height(400)
+        .colors(d3.scale.ordinal().range(["#91B905", "#D6323D", "#014B45", "#8B8C8A", "#12A853", "#00A3DF"]))
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .transitionDuration(1500)
+        .elasticX(true)
+        .ordering(function(d) { return +d.value })
+        .group(group);
+}
+
+function show_data_table(ndx) {
+
+    var dim = ndx.dimension(function(d) { return d.dim; });
+
+    var table = dc.dataTable("#dc-data-table") /* variable created for pagination */
+
+        .dimension(dim)
+        .group(function(d) { return ""; })
+        .size(Infinity) /* Adjust amount of rows here. Use 'Infinity' to show all data */
+
+        .columns([
+            function(d) { return d.Constituency_Name; },
+            function(d) { return d.Candidate; },
+            function(d) { return d.Gender; },
+            function(d) { return d.Party; },
+            // function(d) { return d.Party_Abbreviation; },
+            function(d) { return d.Result; },
+            function(d) { return d.Count_1; },
+            function(d) { return d.Total_Votes; }
+
+        ]).sortBy(function(d) {
+            return d.Constituency_Name; /* sortBy return = d.Constituency_Name will sort data by Constituency_Names */
+        })
+        .order(d3.ascending)
+
+        /* pagination */
+
+        .on('preRender', update_offset)
+        .on('preRedraw', update_offset)
+        .on('pretransition', display);
+
+
+
+    var ofs = 0,
+        pag = 7;
+
+    function update_offset() {
+        var totFilteredRecs = ndx.groupAll().value();
+        var end = ofs + pag > totFilteredRecs ? totFilteredRecs : ofs + pag;
+        ofs = ofs >= totFilteredRecs ? Math.floor((totFilteredRecs - 1) / pag) * pag : ofs;
+        ofs = ofs < 0 ? 0 : ofs;
+        table.beginSlice(ofs); /*table used as variable for dc.dataTable("#dc-data-table") */
+        table.endSlice(ofs + pag); /*table used as variable for dc.dataTable("#dc-data-table")*/
+    }
+
+    function display() {
+        var totFilteredRecs = ndx.groupAll().value();
+        var end = ofs + pag > totFilteredRecs ? totFilteredRecs : ofs + pag;
+        d3.select('#begin')
+            .text(end === 0 ? ofs : ofs + 1);
+        d3.select('#end')
+            .text(end);
+        d3.select('#last')
+            .attr('disabled', ofs - pag < 0 ? 'true' : null);
+        d3.select('#next')
+            .attr('disabled', ofs + pag >= totFilteredRecs ? 'true' : null);
+        d3.select('#size').text(totFilteredRecs);
+        if (totFilteredRecs != ndx.size()) {
+            d3.select('#totalsize').text("(filtered Total: " + ndx.size() + " )");
+        }
+        else {
+            d3.select('#totalsize').text('');
+        }
+    }
+
+    $('#next').on('click', function() {
+        ofs += pag;
+        update_offset();
+        table.redraw();
+    });
+
+
+
+    $('#last').on('click', function() {
+        ofs -= pag;
+        update_offset();
+        table.redraw();
+    });
+
 }
